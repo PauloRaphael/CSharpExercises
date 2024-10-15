1ec8cdbfcd4d498906938150ef1297fa66813f49
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 10:17:28 -0300

    Files Execise

---
+++ ExerciseFiles/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseFiles.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            this.quantity = quantity;
+        }
+    }
+}
---
+++ ExerciseFiles/Program.cs
@@ -0,0 +1,45 @@
+﻿using ExerciseFiles.Entities;
+using System.Globalization;
+
+namespace ExerciseFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Full File Path: ");
+            string fullFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(fullFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(fullFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\Summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                foreach (string line in lines)
+                {
+                    using (StreamWriter sw = File.AppendText(targetFilePath))
+                    {
+                        string[] fields = line.Split(';');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product.Name + ";" + product.Price.ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
