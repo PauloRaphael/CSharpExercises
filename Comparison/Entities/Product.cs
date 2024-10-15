480a858ac2fb5ffd1290a4a6df0917b6470e6a25
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 10:15:34 -0300

    Comparison Exercise

---
+++ Comparison/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Entities
+{
+    internal class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(String name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Comparison/Program.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+List<Product> products = new List<Product>();
+
+products.Add(new Product("TV", 900.00));
+products.Add(new Product("Notebook", 1200.00));
+products.Add(new Product("Tablet", 450.00));
+
+products.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+foreach(Product product in products)
+{
+    Console.WriteLine(product);
+}
