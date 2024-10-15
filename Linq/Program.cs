442d4f642131790fb545ef7797069a4f46657acc
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 10:18:41 -0300

    Linq Exercise

---
+++ Linq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+//data source
+int[] numbers = { 2, 3, 4, 5 };
+
+// query
+IEnumerable<int> result = numbers
+    .Where(x => x % 2 == 0)
+    .Select(x => x * 10);
+
+//Execute the query
+
+foreach(int x in result)
+{
+    Console.WriteLine(x);
+}   
