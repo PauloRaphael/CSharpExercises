b97f5e054dfc92bab038355470bc6e7ba28deb0b
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 10:35:56 -0300

    Guessing Game Exercise

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,38 @@
+﻿Random rand = new Random();
+
+int numeroAleatorio = rand.Next(1, 100);
+
+int tentativas = 0;
+
+while (true)
+{
+    Console.Write("Digite um numero entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+    tentativas++;
+
+    if (chute < numeroAleatorio)
+    {
+        Console.WriteLine("Tente um numero maior.");
+
+    }
+    else if (chute > numeroAleatorio)
+    {
+        Console.WriteLine("Tente um numero menor");
+    }
+    else
+    {
+        Console.WriteLine("Parabens! você acertou o numero em {0} tentativas!", tentativas);
+        Console.WriteLine("jogar de novo? ");
+        char jogardenovo = char.Parse(Console.ReadLine());
+        if (jogardenovo.Equals('y'))
+        {
+            Console.WriteLine("Reiniciando o jogo...");
+            numeroAleatorio = rand.Next(1, 100);
+            tentativas = 0;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
