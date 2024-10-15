c781494372ac327690dd08a05a199d5814663872
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 10:56:45 -0300

    One Line Factorial Exercise

---
+++ OneLineFactorial/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OneLineFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Factorial(5));
+        }
+        static int Factorial(int x) => (x == 0) ? 1 : x * Factorial(x - 1);
+    }
+}
